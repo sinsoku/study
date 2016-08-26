c45067f50638a06e6212e75d862fde7a807bbc9e
sinsoku <sinsoku.listy@gmail.com>
2016-08-26 14:06:19 +0900

    :lock: Add constraints and update schema.rb

--- rails/cable_blog/db/migrate/20160826050054_create_organizations.rb
+++ rails/cable_blog/db/migrate/20160826050054_create_organizations.rb
@@ -1,7 +1,7 @@
 class CreateOrganizations < ActiveRecord::Migration[5.0]
   def change
     create_table :organizations do |t|
-      t.string :name
+      t.string :name, null: false, index: { unique: true }
 
       t.timestamps
     end
--- rails/cable_blog/db/schema.rb
+++ rails/cable_blog/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826035052) do
+ActiveRecord::Schema.define(version: 20160826050054) do
+
+  create_table "organizations", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_organizations_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
