b8e2d0091f3652f59bc86d4789d24fb7b8cbdaac
sinsoku <sinsoku.listy@gmail.com>
2016-11-01 20:13:23 +0900

    Implement the parse_args method

---
+++ elixir/issues/lib/issues/cli.ex
@@ -0,0 +1,40 @@
+defmodule Issues.CLI do
+
+  @default_count 4
+
+  @moduledoc """
+  Handle the comand line parsing and the dispatch to
+  the various functions that end up generating a
+  table of the last _n_ issues in a github project
+  """
+
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help.
+
+  Otherwise it is a github use name, project name, and (optionally)
+  the number of entries to format.
+  Return a tuple of `{ user, project, cunt}`, or `:help` if help was given.
+  """
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [ help: :boolean],
+                                     aliases:  [ h:    :help   ])
+    case parse do
+
+    { [ help: true ], _, _ }
+      -> :help
+
+    { _, [ user, project, count ], _ }
+      -> { user, project, count }
+
+    { _, [user, project ], _ }
+      -> { user, project, @default_count }
+
+    _ -> :help
+
+    end
+  end
+end
