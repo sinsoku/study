2ba262eec66b801ae163839df8b20349fbf081f1
sinsoku <sinsoku.listy@gmail.com>
2016-11-01 20:46:27 +0900

    Add a sample that does not stop

---
+++ elixir/spawn/spawn2.ex
@@ -0,0 +1,23 @@
+defmodule Spawn2 do
+  def greet do
+    receive do
+      {sender, msg} ->
+        send sender, { :ok, "Hello, #{msg}" }
+    end
+  end
+end
+
+pid = spawn(Spawn2, :greet, [])
+
+send pid, {self, "World!"}
+
+receive do
+  {:ok, message} ->
+    IO.puts message
+end
+
+send pid, {self, "Kermit!"}
+receive do
+  {:ok, message} ->
+    IO.puts message
+end
