db0a6ccebdaac8b0809db74ecab96fddb2d8c335
sinsoku <sinsoku.listy@gmail.com>
2016-11-01 20:19:43 +0900

    [wip] Implement process method for :help

--- elixir/issues/lib/issues/cli.ex
+++ elixir/issues/lib/issues/cli.ex
@@ -9,7 +9,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -36,5 +38,16 @@ defmodule Issues.CLI do
     _ -> :help
 
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
