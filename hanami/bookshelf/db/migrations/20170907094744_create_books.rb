117be88b1c4500c2c2ca266afd07192097d3e656
sinsoku <sinsoku.listy@gmail.com>
2017-09-07 18:54:52 +0900

    :sparkles: Add columns

--- hanami/bookshelf/db/migrations/20170907094744_create_books.rb
+++ hanami/bookshelf/db/migrations/20170907094744_create_books.rb
@@ -3,6 +3,9 @@ Hanami::Model.migration do
     create_table :books do
       primary_key :id
 
+      column :title,  String, null: false
+      column :author, String, null: false
+
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
     end
